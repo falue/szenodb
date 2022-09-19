d0e9e01ff91fad746b4230f195e73b59c667a5ce
falue <info@fluescher.ch>
2022-09-19 22:10:35 +0200

    Opacity is now an attrobute of Copy element

--- src/components/Copy.vue
+++ src/components/Copy.vue
@@ -1,7 +1,7 @@
 <template>
   <v-tooltip :disabled="$vuetify.breakpoint.smAndDown" :bottom="!position" :right="position === 'right'" :left="position === 'left'" :top="position === 'top'" color="#212121">
     <template v-slot:activator="{ on, attrs }">
-        <v-icon class="hover" :class="[iconClasses ? iconClasses : 'grey--text ml-2', $vuetify.breakpoint.smAndDown ? 'bigMobileButton' : '']" :small="$vuetify.breakpoint.mdAndUp" v-bind="attrs" v-on="on"
+        <v-icon class="hover" :style="opacity ? `opacity: ${opacity} !important;` : `opacity: .25 !important;`" :class="[iconClasses ? iconClasses : 'grey--text ml-2', $vuetify.breakpoint.smAndDown ? 'bigMobileButton' : '']" :small="$vuetify.breakpoint.mdAndUp" v-bind="attrs" v-on="on"
                 @click="$helpers.copyClipBoard(data, $helpers.capitalize(dataName))">mdi-content-copy
         </v-icon>
     </template>
@@ -11,12 +11,11 @@
 
 <script>
   export default {
-    props: ['data', 'dataName', 'iconClasses', 'position'],
+    props: ['data', 'dataName', 'iconClasses', 'position', 'opacity'],
   }
 </script>
 
 <style scoped>
-  .hover { opacity: .25 !important;}
   .hover:hover { opacity: 1 !important;}
   .bigMobileButton { transform: translateY(-.25em); padding: .25em .5em; line-height: 1em; }
 </style>
