aefa416abe3aa7a6657c666ac76e4fa324a8a5f3
falue <info@fluescher.ch>
2022-09-22 22:59:27 +0200

    Add deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Create build
+npm run build &&
+cd ./dist &&
+
+# Copy specified dir recursively
+# use port 2121 as per metanet. get IP from metanet gui. foldername of website.
+scp -P 2121 -r ./ filmkulissen@80.74.158.100:/szenodb.telefabi.ch
+
+
+# NOTES
+
+# login manually to server:
+# ssh <username>@80.74.158.100 -p2121
+
+# get authrized ssh keys /home/<username>/.ssh:
+# https://download.asperasoft.com/download/docs/ascp/3.5.2/html/dita/creating_public_key_cmd.html
+
+# Save ssh key in file /.ssh/authorized_keys on server 
+# https://www.metanet.ch/support/713
+
+# following copies only the specified file
+#scp ./otherdir/myfile user@servername:/final/path/otherdir/
