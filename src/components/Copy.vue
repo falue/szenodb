d38a4056065028b1bb70e2d50fa8d8f86350bbcb
falue <info@fluescher.ch>
2022-09-26 13:48:36 +0200

    Copy component now can have explicit position of "bottom"

--- src/components/Copy.vue
+++ src/components/Copy.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-tooltip :disabled="$vuetify.breakpoint.smAndDown" :bottom="!position" :right="position === 'right'" :left="position === 'left'" :top="position === 'top'" color="#212121">
+  <v-tooltip :disabled="$vuetify.breakpoint.smAndDown" :bottom="!position || position === 'bottom'" :right="position === 'right'" :left="position === 'left'" :top="position === 'top'" color="#212121">
     <template v-slot:activator="{ on, attrs }">
         <v-icon class="hover" :style="opacity ? `opacity: ${opacity} !important;` : `opacity: .25 !important;`" :class="[iconClasses ? iconClasses : 'grey--text ml-2', $vuetify.breakpoint.smAndDown ? 'bigMobileButton' : '']" :small="$vuetify.breakpoint.mdAndUp" v-bind="attrs" v-on="on"
                 @click="$helpers.copyClipBoard(data, $helpers.capitalize(dataName))">mdi-content-copy
