fb9780801a7fcb75542e532054d354a928df20ee
falue <info@fluescher.ch>
2022-10-16 21:17:33 +0200

    Add dicebear as external services in FAAdd dicebear as external services in FAQ

--- src/views/About.vue
+++ src/views/About.vue
@@ -287,6 +287,7 @@ import Copy from '@/components/Copy'
                       Dynamic image storage by <a href="https://firebase.google.com/products/storage" target="_blank">Google Firebase Storage</a></p><p>
                       Hosting by <a href="https://metanet.ch" target="_blank">metanet.ch</a></p><p>
                       Dynamic translations by <a href="https://deepl.com" target="_blank">Deepl.com</a></p><p>
+                      Random default user image API by <a href="https://avatars.dicebear.com" target="_blank">dicebear.com</a></p><p>
                       No tracking/analytics tools are being used.
                       `
           },
